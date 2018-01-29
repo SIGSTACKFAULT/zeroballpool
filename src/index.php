2e25c1a2ed81809bb50861ec53ec196d45b26f40
Connor [REDACTED] <blacksilverck35@gmail.com>
2018-01-29 23:36:03 +0000

    index.php

---
+++ src/index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+	<head>
+		<title>Zero ball pool.</title>
+		<link href="https://fonts.googleapis.com/css?family=Lobster|Roboto" rel="stylesheet">
+		<link href="zbp.css" rel="stylesheet">
+	</head>
+	<body>
+		<h1>Zero ball pool</h1>
+		<p>My latest overambitious project</p>
+	</body>
+</html>
