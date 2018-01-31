9cee6b03fbaf8853c812799b8383298f9055a3d7
Connor "Blacksilver" [REDACTED] <blacksilverck35@gmail.com>
2018-01-31 16:58:52 +0000

    Gotta have periods

--- src/common/head.php
+++ src/common/head.php
@@ -1,6 +1,6 @@
 <!-- FILE: common/head.php -->
 <div id="head">
-	<h1>Zero ball pool</h1>
+	<h1>Zero ball pool.</h1>
 	<p><a href="http://blacksilver.xyz">My</a> latest overambitious project.</p>
 </div>
 <!-- /FILE -->
