2467934a756e3502888a42248cabb0c7c24a7f4e
Connor "Blacksilver" [REDACTED] <blacksilverck35@gmail.com>
2018-02-01 16:07:54 +0000

    Placeholders for recent and popular

--- src/index.php
+++ src/index.php
@@ -6,5 +6,27 @@
 	</head>
 	<body>
 <?php include($_SERVER["DOCUMENT_ROOT"] . "/common/header.php"); ?>
+		<div id="content">
+			<h2>Popular
+				<img	src="https://upload.wikimedia.org/wikipedia/commons/c/c7/Loading_2.gif"
+					alt="Loading..."
+					width=20
+					class="loading">
+			</h2>
+			<ul id="popular-pools">
+				<li>Placeholder</li>
+			</ul>
+			
+			<hr>
+			<h2>Recent
+				<img	src="https://upload.wikimedia.org/wikipedia/commons/c/c7/Loading_2.gif"
+					alt="Loading..."
+					width=20
+					class="loading">
+			</h2>
+			<ul id="recent-pools">
+				<li>Placeholder</li>
+			</ul>
+		</div>
 	</body>
 </html>
