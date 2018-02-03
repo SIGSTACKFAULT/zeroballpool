57e51c795cd28dc845244f22394329e31ce10a9f
Connor "Blacksilver" [REDACTED] <blacksilverck35@gmail.com>
2018-02-03 04:35:20 +0000

    404 page

---
+++ src/404.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+	<head>
+		<title>404 | Zero ball pool.</title>
+<?php include($_SERVER["DOCUMENT_ROOT"] . "/common/head.php") ?>
+	<link href="/404.css" rel="stylesheet">
+	</head>
+	<body>
+<?php include($_SERVER["DOCUMENT_ROOT"] . "/common/header.php"); ?>
+	
+	<h1 id="err">404</h1>
+	<p  id="sub">There is no page here :(</p>
+
+	<hr>
+	
+	<p id="hint">What else do you expect this page to say?</p>
+
+	</body>
+</html>
