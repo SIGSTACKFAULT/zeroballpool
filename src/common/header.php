2e9349c3328b3f300ae763fc3de9aa938c3041c7
Connor "Blacksilver" [REDACTED] <blacksilverck35@gmail.com>
2018-02-03 05:04:23 +0000

    delete comment from github:buttons

--- src/common/header.php
+++ src/common/header.php
@@ -3,7 +3,6 @@
 	<h1><a href="http://zeroballpool.com">Zero ball pool.</a></h1>
 	<p><a href="http://blacksilver.xyz">My</a> latest overambitious project.</p>
 	<div id="gh-button">
-<!-- Place this tag where you want the button to render. -->
 <a class="github-button" href="https://github.com/Blacksilver42/zeroballpool/fork" data-icon="octicon-repo-forked" data-size="large" data-show-count="true" aria-label="Fork Blacksilver42/zeroballpool on GitHub">Fork</a> 
 	</div>
 </div>
