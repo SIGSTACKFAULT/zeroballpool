1def7b51404142c9124619e07222728254acca2f
Connor "Blacksilver" [REDACTED] <blacksilverck35@gmail.com>
2018-02-08 18:07:26 +0000

    changed GH button

--- src/common/header.php
+++ src/common/header.php
@@ -3,7 +3,7 @@
 	<h1><a href="http://zeroballpool.com">Zero ball pool.</a></h1>
 	<p><a href="http://blacksilver.xyz">My</a> latest overambitious project.</p>
 	<div id="gh-button">
-<a class="github-button" href="https://github.com/Blacksilver42/zeroballpool/fork" data-icon="octicon-repo-forked" data-size="large" data-show-count="true" aria-label="Fork Blacksilver42/zeroballpool on GitHub">Fork</a> 
+<a title="For a GNU dawn!" class="github-button" href="https://github.com/Blacksilver42/zeroballpool" data-size="large" data-show-count="true" aria-label="Star Blacksilver42/zeroballpool on GitHub">Star</a>
 	</div>
 </div>
 <!-- /FILE -->
